ce96047189857d0592de8e04fea627a30062bd3f
Jehan Afwazi A <jee.archer@gmail.com>
2020-04-17 18:50:41 +0700

    update route and readme

--- README.md
+++ README.md
@@ -5,5 +5,16 @@ the data source from kompas.com and corona.temanggung.go.id.
 
 this application will be auto updated every day at 16.00 WIB
 
+The Route included :
 
-4 Fun Only, enjoyed!!
+- GET("/")  -- home
+- GET("/provinces")   -- get cases in province
+- GET("/provinces/latest") -- get latest update cases in province
+- GET("/provinces/populate")  -- populate data province
+- GET("/temanggungs") -- get cases in temanggung
+- GET("/temanggungs/latest") -- get latest cases in temanggung
+- GET("/temanggungs/populate") -- populate data in temanggung
+- GET("/populate") -- crawl all 
+
+
+~ 4 Fun Only, enjoyed!! ~
--- routes/routes.go
+++ routes/routes.go
@@ -13,13 +13,13 @@ func Route() *gin.Engine {
 		v1.GET("/", handlers.Home)
 		v1.GET("/provinces", handlers.GetAllCasesInProvince)
 		v1.GET("/provinces/latest", handlers.LatestCasesInProvince)
-		v1.GET("/provinces/crawl", handlers.CrawlInProvince)
+		v1.GET("/provinces/populate", handlers.CrawlInProvince)
 
 		v1.GET("/temanggungs", handlers.GetAllCaseInTemanggung)
 		v1.GET("/temanggungs/latest", handlers.GetLatestCasesInTemanggung)
-		v1.GET("/temanggungs/crawl", handlers.CrawlTmg)
+		v1.GET("/temanggungs/populate", handlers.CrawlTmg)
 
-		v1.GET("/crawl", handlers.CrawlAll)
+		v1.GET("/populate", handlers.CrawlAll)
 	}
 
 	return r
